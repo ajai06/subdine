60bf95d30f938fd9676ae9a6a54067eeeb4bf8ec
ajaisajin <ajaisajin06@gmail.com>
2019-08-27 22:34:57 +0530

    'initial'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TestingService } from './testing.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'subdine';
+
+  constructor(private formBuilder: FormBuilder,
+              private testingService: TestingService) {}
+
+
+ testForm = this.formBuilder.group({
+   name: ['Test Item2'],
+   taxes_id: [17],
+   units_id: [1],
+   base: [750],
+   is_perishable: [],
+   is_negative: [],
+   item_details: this.formBuilder.group({
+     price: [10],
+     hsn_code: [],
+     min: [2],
+     max: [10],
+     Branches_id: [16]
+   }),
+   category_ids: [[116]]
+ });
+
+onSubmit() {
+  console.log(this.testForm.value);
+  this.testingService.test(this.testForm.value).subscribe(
+    res => {
+      console.log(res);
+    },
+    err => {}
+  );
+}
 }
---
+++ src/app/testing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestingService {
+
+  baseUrl: ' http://dev-items.subdineapis.com/items';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      users_id: '23',
+      // tslint:disable-next-line: max-line-length
+      token: '1quOzEtlGvcORSv8esLtcVM6kmcRKGhcMrNgmwEUTeNoSn0ETJW5l7iGsUyYNGZ2GFgIFl8t3Yvs8p4nF0Yx8ceIASxIN8SbrGxKOtCv2Oa1uWPbxIb6qF2C8HpSKXqI1D9HyPyPDztIISnXuG4yeVNfFs2LPrCe34j9E6t7Lo5kCFXLNLi0UcW1WaUI0Rqs3OFvHpyn'
+
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  test(testForm: any) {
+    return this.http.post<any>(this.baseUrl, this.httpOptions, testForm);
+  }
+
+}
